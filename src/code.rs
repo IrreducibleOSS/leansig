eab6d7b1079cabef10eadd69cc4f340469dfdc88
Sergei Shulepov <s.pepyakin@gmail.com>
2025-08-06 19:54:15 +0200

    cleanup

--- src/code.rs
+++ src/code.rs
@@ -3,18 +3,21 @@
 use bitvec::prelude::*;
 use rand::rngs::StdRng;
 
-use crate::{Message, Param, Rho, hash::tweak_hash_message, spec::Spec};
+use crate::{Message, Nonce, Param, hash::tweak_hash_message, spec::Spec};
 
-/// Find a suitable encoding to fit into the target sum.
+/// Try to find a suitable encoding to fit into the target sum.
+///
+/// For this we are going to try different random parameter values until we find a valid encoding.
+/// It should not take too many iterations, but in case it does, we will give up and return `None`.
 pub fn grind(
     spec: &Spec,
     max_retries: usize,
     param: &Param,
     message: &Message,
     rng: &mut StdRng,
-) -> Option<(Codeword, Rho)> {
+) -> Option<(Codeword, Nonce)> {
     for _ in 0..max_retries {
-        let rho = Rho::random(rng);
+        let rho = Nonce::random(rng);
         match new_valid(spec, param, message, &rho) {
             Some(codeword) => return Some((codeword, rho)),
             None => continue,
@@ -24,8 +27,10 @@ pub fn grind(
     None
 }
 
-pub fn new_valid(spec: &Spec, param: &Param, message: &Message, rho: &Rho) -> Option<Codeword> {
-    let codeword = Codeword::new(spec, param, message, rho);
+/// Creates a new codeword and returns `Some` only if the codeword valid, that is, the sum
+/// of chunks is equal to the target sum dictated by the spec.
+pub fn new_valid(spec: &Spec, param: &Param, message: &Message, nonce: &Nonce) -> Option<Codeword> {
+    let codeword = Codeword::new(spec, param, message, nonce);
     if codeword.sum() == spec.target_sum {
         Some(codeword)
     } else {
@@ -33,57 +38,68 @@ pub fn new_valid(spec: &Spec, param: &Param, message: &Message, rho: &Rho) -> Op
     }
 }
 
+/// Codeword is basically a coordinate on this hypercube structure.
+///
+/// The origin of this structure is where the private key is stored.
 pub struct Codeword {
-    chunks: Vec<u8>,
+    coords: Vec<u8>,
 }
 
 impl Codeword {
-    pub fn new(spec: &Spec, param: &Param, message: &Message, rho: &Rho) -> Codeword {
-        let full_hash = tweak_hash_message(param, message, rho);
-        let trunc_hash = &full_hash.as_ref()[0..spec.num_chains * spec.chunk_bits / 8];
-        let chunks = bytes_to_chunks(trunc_hash, spec.chunk_bits);
-        Self { chunks }
+    pub fn new(spec: &Spec, param: &Param, message: &Message, nonce: &Nonce) -> Codeword {
+        let full_hash = tweak_hash_message(param, message, nonce);
+        let trunc_hash = &full_hash.as_ref()[0..spec.message_hash_len];
+        let coords = bytes_to_coordinates(trunc_hash, spec.coordinate_resolution_bits);
+        assert_eq!(coords.len(), spec.dimension());
+        Self { coords }
     }
 
-    /// Returns the sum over all the chunks.
+    /// Returns the sum over all the coordinates.
     ///
-    /// This defines the distance from the sink
+    /// You can think about it as a distance from the source, the where the secret key is stored.
+    ///
+    /// In our use case, this is the number of hashes required to get from the secret key to the
+    /// message and for the efficiency of verifier we want to minimize this.
     pub fn sum(&self) -> usize {
-        self.chunks.iter().map(|&chunk| chunk as usize).sum()
+        self.coords
+            .iter()
+            .map(|&coordinate| coordinate as usize)
+            .sum()
     }
 
-    pub fn num_chunks(&self) -> usize {
-        self.chunks.len()
+    pub fn dimension(&self) -> usize {
+        self.coords.len()
     }
 
-    pub fn chunks(&self) -> &[u8] {
-        &self.chunks
+    pub fn coords(&self) -> &[u8] {
+        &self.coords
     }
 }
 
-fn bytes_to_chunks(bytes: &[u8], chunk_bits: usize) -> Vec<u8> {
-    assert!(chunk_bits <= 8);
-    assert!(chunk_bits.is_power_of_two());
+/// Chops bytes into coordinates of a given resolution.
+fn bytes_to_coordinates(bytes: &[u8], resolution_bits: usize) -> Vec<u8> {
+    assert!(resolution_bits <= 8);
+    assert!(resolution_bits.is_power_of_two());
     bytes
         .view_bits::<Lsb0>()
-        .chunks_exact(chunk_bits)
-        .map(|chunk| chunk.load::<u8>())
+        .chunks_exact(resolution_bits)
+        .map(|coordinate| coordinate.load::<u8>())
         .collect::<Vec<u8>>()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::bytes_to_chunks;
+    use super::bytes_to_coordinates;
 
     #[test]
-    fn test_bytes_to_chunks() {
-        let chunks = bytes_to_chunks(&[0b01101100], 2);
-        assert_eq!(chunks, vec![0b00, 0b11, 0b10, 0b01]);
+    fn test_bytes_to_coordinates() {
+        let coords = bytes_to_coordinates(&[0b01101100], 2);
+        assert_eq!(coords, vec![0b00, 0b11, 0b10, 0b01]);
     }
 
     #[test]
     fn test_full_byte() {
-        let chunks = bytes_to_chunks(&[0b01101100, 0b10100110], 8);
-        assert_eq!(chunks, vec![0b01101100, 0b10100110]);
+        let coords = bytes_to_coordinates(&[0b01101100, 0b10100110], 8);
+        assert_eq!(coords, vec![0b01101100, 0b10100110]);
     }
 }
--- src/spec.rs
+++ src/spec.rs
@@ -1,12 +1,11 @@
 /// Specification for the signature scheme instantiation.
 #[derive(Clone, Debug)]
 pub struct Spec {
-    /// The number of bits per chunk.
-    pub chunk_bits: usize,
-    /// The number of chunks in a codeword.
+    pub message_hash_len: usize,
+    /// The number of bits per each coordinate in a codeword.
     ///
-    /// Also number of chunks.
-    pub num_chains: usize,
+    /// This is denoted by `w` in the paper or known as chunk size in the reference implementation.
+    pub coordinate_resolution_bits: usize,
     /// The length of the parameter for hashing.
     pub param_len: usize,
     /// The sum of all coordinates of a vertex of a signature that we accept.
@@ -14,27 +13,29 @@ pub struct Spec {
 }
 
 impl Spec {
-    /// Returns the number of chains (same as num_chunks).
-    pub fn num_chains(&self) -> usize {
-        self.num_chains
+    /// The dimension of the hypercube.
+    ///
+    /// This is the same as the number of chains.
+    pub fn dimension(&self) -> usize {
+        self.message_hash_len * 8 / self.coordinate_resolution_bits
     }
 
     /// Returns the chain length (2^chunk_bits).
     pub fn chain_len(&self) -> usize {
-        1 << self.chunk_bits
+        1 << self.coordinate_resolution_bits
     }
 }
 
-impl Default for Spec {
-    fn default() -> Self {
-        let message_hash_len = 18;
-        let chunk_bits = 4;
-        let num_chains = message_hash_len * 8 / chunk_bits;
-        Self {
-            chunk_bits,
-            num_chains,
-            param_len: 18,
-            target_sum: 297,
-        }
-    }
-}
+pub const SPEC_1: Spec = Spec {
+    message_hash_len: 18,
+    coordinate_resolution_bits: 2,
+    param_len: 18,
+    target_sum: 119,
+};
+
+pub const SPEC_2: Spec = Spec {
+    message_hash_len: 18,
+    coordinate_resolution_bits: 4,
+    param_len: 18,
+    target_sum: 297,
+};
